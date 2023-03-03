a837edd9d0fe2127fde0650800a342b105b6905a
JangiGabmeister <109713605+JangiGabmeister@users.noreply.github.com>
2023-03-03 17:53:47 +1100

    updated character movement
    early stages of allowing player model to turn 180 degrees when moving side to side

--- 2.5D Beetle Game/Assets/PlayerMovement.cs
+++ 2.5D Beetle Game/Assets/PlayerMovement.cs
@@ -13,8 +13,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Speeds")]
     public float movementSpeed = 10f;
     public float jumpSpeed = 5f;
-    public float gravity = 10f;
-    public float rotationSpeed;
+    float gravity = 9.8f;
 
     Vector3 _movementDirection;
     Vector3 _rotationChange;
@@ -27,19 +26,31 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         if (controller.isGrounded)
-        {
+        { 
             float horizontaInput = Input.GetAxis("Horizontal");
 
             _movementDirection = transform.TransformDirection(new Vector3(horizontaInput, 0, 0)) * movementSpeed;
 
+            if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+            {
+                playerCapsule.right = Vector3.Lerp(playerCapsule.right, -playerCapsule.right, Time.deltaTime * 1000);
+            }
+
+            //if player presses forward button, rotates player model 90 degrees to the left
             if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
             {
-                player.Rotate(_rotationChange);
+                player.Rotate(-_rotationChange);
             }
+            //if player presses back button, rotates player model 90 degrees right
             else if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
             {
-                player.Rotate(-_rotationChange);
+                player.Rotate(_rotationChange);
             }
 
             if (Input.GetButton("Jump"))
