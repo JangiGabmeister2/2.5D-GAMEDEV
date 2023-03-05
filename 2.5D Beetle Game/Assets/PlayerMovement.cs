d0bcce6446fdfb47713f2fbaf39ae277da947c98
JangiGabmeister <109713605+JangiGabmeister@users.noreply.github.com>
2023-03-05 14:47:58 +1100

    updated player movement

--- 2.5D Beetle Game/Assets/PlayerMovement.cs
+++ 2.5D Beetle Game/Assets/PlayerMovement.cs
@@ -5,52 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
-    public CharacterController controller;
-    public Transform orientation;
+    float _playerHeight;
     public Transform playerCapsule;
-    public Transform player;
 
     [Header("Movement Speeds")]
     public float movementSpeed = 10f;
     public float jumpSpeed = 5f;
     float gravity = 9.8f;
 
+    [Header("Surface Alignment")]
+    public LayerMask groundLayer;
+
     Vector3 _movementDirection;
     Vector3 _rotationChange;
 
+    CharacterController _charC;
     private void Start()
     {
-        controller = GetComponent<CharacterController>();
-        _rotationChange = new Vector3(0, 90, 0);
+        _charC = GetComponent<CharacterController>();
+        _playerHeight = _charC.height;
+
+        _rotationChange = new Vector3(0, 45, 0);
     }
 
     private void Update()
     {
         MovePlayer();
+        //SurfaceAlignment();
     }
 
     private void MovePlayer()
     {
-        if (controller.isGrounded)
-        { 
+        if (IsGrounded())
+        {
             float horizontaInput = Input.GetAxis("Horizontal");
 
             _movementDirection = transform.TransformDirection(new Vector3(horizontaInput, 0, 0)) * movementSpeed;
 
-            if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
-            {
-                playerCapsule.right = Vector3.Lerp(playerCapsule.right, -playerCapsule.right, Time.deltaTime * 1000);
-            }
-
-            //if player presses forward button, rotates player model 90 degrees to the left
+            //if player presses forward button, rotates player model 45 degrees to the left
             if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") > 0)
             {
-                player.Rotate(-_rotationChange);
+                transform.Rotate(-_rotationChange);
             }
-            //if player presses back button, rotates player model 90 degrees right
             else if (Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0)
             {
-                player.Rotate(_rotationChange);
+                transform.Rotate(_rotationChange);
             }
 
             if (Input.GetButton("Jump"))
@@ -60,7 +59,28 @@ public class PlayerMovement : MonoBehaviour
         }
 
         _movementDirection.y -= gravity * Time.deltaTime;
-        controller.Move(_movementDirection * Time.deltaTime);
+        _charC.Move(_movementDirection * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, _playerHeight, groundLayer))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void SurfaceAlignment()
+    {
+        Ray ray = new Ray(playerCapsule.position, -transform.up);
+        RaycastHit info = new RaycastHit();
+
+        if (Physics.Raycast(ray, out info, groundLayer))
+        {
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, info.normal);
+        }
     }
 }
 
