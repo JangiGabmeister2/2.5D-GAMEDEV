e7614f8a2576a6f60b4057e4f326d96caae78c68
JangiGabmeister2 <109714078+JangiGabmeister2@users.noreply.github.com>
2023-03-08 16:05:14 +1100

    changed camera settings

--- 2.5D Beetle Game/Assets/CameraFollow.cs
+++ 2.5D Beetle Game/Assets/CameraFollow.cs
@@ -4,32 +4,5 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    //the target which the camera is following and focusing on
-    [Tooltip("The target which the camera is following and focusing on.")]
-    public Transform target;
-    //smoothly follows the target
-    [Tooltip("How smooth the camera follows the target.")]
-    public float smoothing = 5f;
-    //distance between camera and target
-    Vector3 _distance;
-
-    Vector3 _cameraPosition;
-    Vector3 _rightPosition;
-
-    private void Start()
-    {
-        _distance = transform.position - target.position;
-    }
-
-    private void LateUpdate()
-    {
-        MoveCamera();
-    }
-
-    private void MoveCamera()
-    {
-        Vector3 targetCamPos = target.position + _distance;
-
-        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
-    }
+    
 }
