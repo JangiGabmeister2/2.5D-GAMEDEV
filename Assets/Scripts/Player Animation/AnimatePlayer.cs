3bba3ab18960bed3400cda41281d09821bc99f4f
JangiGabmeister <109713605+JangiGabmeister@users.noreply.github.com>
2023-03-17 14:41:18 +1100

    commit

--- Assets/Scripts/Player Animation/AnimatePlayer.cs
+++ Assets/Scripts/Player Animation/AnimatePlayer.cs
@@ -7,7 +7,10 @@ public class AnimatePlayer : MonoBehaviour
     Animator _playerAnimator;
     SpriteRenderer _renderer;
 
+<<<<<<< Updated upstream
     //an id for the animator controller's bool parameters
+=======
+>>>>>>> Stashed changes
     int isMoving;
     int isJumping;
 
@@ -16,10 +19,13 @@ public class AnimatePlayer : MonoBehaviour
         _playerAnimator = GetComponent<Animator>();
         _renderer = GetComponent<SpriteRenderer>();
 
+<<<<<<< Updated upstream
         //allows the sprite renderer to cast shadows
         _renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
 
         //connects the bool id to the animator's bool parameters
+=======
+>>>>>>> Stashed changes
         isMoving = Animator.StringToHash("isMoving");
         isJumping = Animator.StringToHash("isJumping");
     }
@@ -31,12 +37,18 @@ public class AnimatePlayer : MonoBehaviour
 
     private void Animate()
     {
+<<<<<<< Updated upstream
         //if player presses either forward, backward, left, or right buttons, return true
         bool _isMoving = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
         //if player presses jump button, returns true
         bool _isJumping = Input.GetButton("Jump");
 
         //flips the sprite's x-rotation according to which direction the player is heading towards
+=======
+        bool _isMoving = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+        bool _isJumping = Input.GetButton("Jump");
+
+>>>>>>> Stashed changes
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             _renderer.flipX = true;
@@ -46,7 +58,10 @@ public class AnimatePlayer : MonoBehaviour
             _renderer.flipX = false;
         }   
 
+<<<<<<< Updated upstream
         //sets the values of bool ids as the above bool's values
+=======
+>>>>>>> Stashed changes
         _playerAnimator.SetBool(isMoving, _isMoving);
         _playerAnimator.SetBool(isJumping, _isJumping);
     }
